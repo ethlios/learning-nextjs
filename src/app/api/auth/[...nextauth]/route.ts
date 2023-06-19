975eb97f74861d895368a7cc951e0127fb104856
Your Name <you@example.com>
2023-06-19 17:14:42 +0700

    test sign in

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -20,7 +20,7 @@ const handler = NextAuth({
             },
             async authorize(credentials, req) {
                 // Add logic here to look up the user from the credentials supplied
-                const res = await fetch('http://localhost:3000/api/login', {
+                const res = await fetch('https://ethlios.vercel.app/api/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
